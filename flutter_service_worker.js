00fe413ffe719eb246e091071ac948a89fa9657b
nishantsharma113 <nishantsharma11398@gmail.com>
2025-06-23 14:20:28 +0530

    commit

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "040ce500a1aabe1ba162725449478535
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "04767c068f31eb999c29f9bb983d8e8c",
+"flutter_bootstrap.js": "32103bf11a7eeb5666661562f23856d7",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
